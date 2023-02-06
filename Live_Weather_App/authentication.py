85657cc1ab5df48086d97d93833915101e82cdfe
Dhruvbam <110248836+Dhruvbam@users.noreply.github.com>
2023-02-06 13:11:40 -0600

    Add files via upload

---
+++ Live_Weather_App/authentication.py
@@ -0,0 +1,7 @@
+import requests
+
+# Authentication Information has been removed from here 
+# Please look at the API Documentation to build your own code here
+
+def request_weather(city):
+    pass
